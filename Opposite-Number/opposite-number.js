62a699aae848b6d061fddff5042c19f009834630
Mohamed Ragab <rmidoragab222@gmail.com>
2024-01-28 20:58:18 +0200

    Add opposite number

---
+++ Opposite-Number/opposite-number.js
@@ -0,0 +1,13 @@
+/**Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).**/
+//---------------------------------------------------------------------------------------------
+
+function opposite(number) {
+  return number * -1;
+}
+console.log(opposite(651));
+/**Another Solution 
+   -------------------------------------------------------------------------------------**/
+function opposite(number) {
+  return -number;
+}
+console.log(opposite(-184));
