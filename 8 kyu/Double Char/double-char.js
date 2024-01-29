2965d9630c74ae9b4615515747af856467999d74
Mohamed Ragab <rmidoragab222@gmail.com>
2024-01-29 23:37:21 +0200

    Add double char

---
+++ 8 kyu/Double Char/double-char.js
@@ -0,0 +1,12 @@
+/**Given a string, you have to return a string in which each character (case-sensitive) is repeated once.**/
+//---------------------------------------------------------------------------------------------
+
+function doubleChar(str) {
+  return str
+    .split("")
+    .map((x) => x.repeat(2))
+    .join("");
+}
+
+// console.log(doubleChar("allooo"));
+
