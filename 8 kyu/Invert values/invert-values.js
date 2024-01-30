cd0229b60d89b3afd4d9773e6fb00d63620cc646
Mohamed Ragab <rmidoragab222@gmail.com>
2024-01-31 00:13:56 +0200

    add Invert values

---
+++ 8 kyu/Invert values/invert-values.js
@@ -0,0 +1,8 @@
+/**Write a function that removes the spaces from the string, then return the resultant string.**/
+//---------------------------------------------------------------------------------------------
+
+function invert(array) {
+  return array.map((x) => -x);
+}
+
+// console.log(invert([1, 2, 3, 8, -8, -5]));
